31c1044eb21ba7beca0c979c176224b2bf988cf6
lavinas <lavinas@me.com>
2024-09-01 19:55:58 -0300

    calculating more in terms of the hours

--- internal/port/dto.go
+++ internal/port/dto.go
@@ -53,7 +53,8 @@ type TruncateOut struct {
 func (r *RunOut) String() string {
 	ret := fmt.Sprintf(stringOutput, r.JobID, r.Shift, r.Status, r.Detail, r.Duration)
 	if r.More != -1 {
-		ret += fmt.Sprintf(". More: %d", r.More)
+		hrs := float64(r.More) * r.Duration / 3600
+		ret += fmt.Sprintf(". More: %d re - %.2f hr", r.More, hrs)
 	}
 	return ret
 }
