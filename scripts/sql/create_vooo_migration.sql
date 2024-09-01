c06dd052122758bc6906bda6ae1d8c7758dac2a9
lavinas <lavinas@me.com>
2024-09-01 11:31:51 -0300

    migration

--- scripts/sql/create_vooo_migration.sql
+++ scripts/sql/create_vooo_migration.sql
@@ -29,9 +29,9 @@ CREATE DATABASE `vooo_migration`;
 USE `vooo_migration`;
 # starting tables
 --- 
---- Table structure for table `schemas`
+--- Table structure for table `schma`
 ---
-DROP TABLE IF EXISTS `schemas`;
+DROP TABLE IF EXISTS `schma`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `schma` (
