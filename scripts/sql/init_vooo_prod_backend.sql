9cc7a0fba9fca4e6f3cdf49229f3afe0e0d74fe2
lavinas <lavinas@me.com>
2024-09-09 22:30:21 -0300

    scripts

--- scripts/sql/init_vooo_prod_backend.sql
+++ scripts/sql/init_vooo_prod_backend.sql
@@ -11,3 +11,14 @@ INSERT INTO `client` (`id`,`economic_group_id`,`id_address`,`id_contact`,`id_doc
 INSERT INTO `user` (`id`,`id_client`,`id_role`,`id_contact`,`headquarter_id`,`id_aggregator`,`username`,`email`,`password`,`status`,`created_at`,`updated_at`,`last_access_date`,`last_password_change_date`) VALUES (1,NULL,1,NULL,NULL,1,'Vooo','vooo@vooo.com.br','$2b$13$7XiJFkFH11aVeTUHnYRCZ.pmpzKjrwJIFrqE2uXneRPr5bTIJk442',1,'2016-10-07 23:28:02','2023-05-29 10:54:03','2024-08-23 17:17:24','2083-08-28');
 INSERT INTO `user_control` (`id`,`client_id`,`user_id`,`time_window_in_minutes`,`password_days_to_expire`,`description`) VALUES (0,NULL,NULL,30,90,'Tempo padrão, caso não tenha customização');
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+## insert no final do processamento
+SET FOREIGN_KEY_CHECKS = 0;
+
+INSERT INTO `aggregator` (`id`,`id_client`,`name`) VALUES (1,0,'Vooo');
+INSERT INTO `document` (`id`,`type`,`number`,`created_at`,`updated_at`) VALUES (0,'CNPJ',23659971000143,'2016-01-01 00:00:00','2016-01-01 00:00:00');
+INSERT INTO `client` (`id`,`economic_group_id`,`id_address`,`id_contact`,`id_document`,`id_author`,`headquarter_id`,`id_aggregator`,`name`,`trading`,`brand`,`status`,`created_at`,`updated_at`) VALUES (0,NULL,NULL,NULL,0,1,NULL,1,'Vooo','Vooo Servicos de Controle e Gestao Ltda','Vooo',0,'2016-01-01 00:00:00','2023-04-20 12:25:56');
+
+INSERT INTO `user` (`id`,`id_client`,`id_role`,`id_contact`,`headquarter_id`,`id_aggregator`,`username`,`email`,`password`,`status`,`created_at`,`updated_at`,`last_access_date`,`last_password_change_date`) VALUES (1,NULL,1,NULL,NULL,1,'Vooo','vooo@vooo.com.br','$2b$13$7XiJFkFH11aVeTUHnYRCZ.pmpzKjrwJIFrqE2uXneRPr5bTIJk442',1,'2016-10-07 23:28:02','2023-05-29 10:54:03','2024-09-09 18:38:10','2083-08-28');
+SET FOREIGN_KEY_CHECKS = 1;
